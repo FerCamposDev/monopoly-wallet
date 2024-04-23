f72c29135f14cc3352fceb800cce3863da92c6f0
fercamposdev <fernando.developments@gmail.com>
2024-04-23 14:42:51 -0300

    save game item

--- apps/front/src/app/commons/enums/storage.enum.ts
+++ apps/front/src/app/commons/enums/storage.enum.ts
@@ -1,3 +1,4 @@
 export enum LocalStorageKey {
   Mute = 'mute',
+  Game = 'game',
 }
--- apps/front/src/app/components/shared/SaveGameItem/index.tsx
+++ apps/front/src/app/components/shared/SaveGameItem/index.tsx
@@ -1,13 +1,52 @@
-import { ListItem, ListItemText } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, ListItem, ListItemText } from "@mui/material";
+import toast from "react-hot-toast";
+import { useGame } from "../../../context/game/useGame";
+import { LocalStorageKey } from "../../../commons/enums/storage.enum";
+import { useToggle } from "../../../hooks";
 
 const SaveGameItem = () => {
+  const { game, player, logs } = useGame();
+  const { open, handleClose, handleOpen } = useToggle();
+
+  const saveGame = () => {
+    const gameData = { game, player, logs };
+    localStorage.setItem(LocalStorageKey.Game, JSON.stringify(gameData));
+    toast.success('Game saved!');
+    handleClose();
+  }
+
+  const handleSaveOrOpen = () => {
+    if (localStorage.getItem(LocalStorageKey.Game)) {
+      return handleOpen();
+    }
+
+    saveGame();
+  };
 
   return (
-    <ListItem onClick={undefined}>
-      <ListItemText>
-        Save Game
-      </ListItemText>
-    </ListItem>
+    <>
+      <ListItem onClick={handleSaveOrOpen}>
+        <ListItemText>
+          Save Game
+        </ListItemText>
+      </ListItem>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+          There is another game saved
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Do you want to replace it with the new game?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button variant='contained' color="error" onClick={saveGame}>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
