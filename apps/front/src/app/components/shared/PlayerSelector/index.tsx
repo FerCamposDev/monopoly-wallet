a06e2403b463e68799408ca1e1b67b1ea9e2bfa6
fercamposdev <fernando.developments@gmail.com>
2024-04-26 23:10:51 -0300

    feat: default user when the game have only two players

--- apps/front/src/app/components/shared/PlayerSelector/index.tsx
+++ apps/front/src/app/components/shared/PlayerSelector/index.tsx
@@ -14,6 +14,10 @@ const PlayerSelector: FC<Props> = ({ onSelect, disabled }) => {
 
   const players = game?.players.filter(p => p.token !== player?.token);
 
+  if (players.length === 1) {
+    onSelect(players[0]);
+  }
+
   return (
     <FormControl fullWidth disabled={disabled}>
       <InputLabel id="player-select-label" disabled={disabled}>
@@ -23,7 +27,7 @@ const PlayerSelector: FC<Props> = ({ onSelect, disabled }) => {
         labelId="player-select-label"
         label="Select Player"
         name="player"
-        defaultValue={''}
+        defaultValue={players.length === 1 ? players[0].token : ''}
         required={!disabled}
         disabled={disabled}
       >
